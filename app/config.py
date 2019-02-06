a021c705df9c02a744b4c7270952650e4694798b
Mugo-Ndungu <twinnymugo@gmail.com>
2019-02-06 10:10:15 +0300

    Add api_key to config file

--- app/config.py
+++ app/config.py
@@ -3,7 +3,10 @@ class Config:
     General configuration parent class
     '''
 
-    NEWS_API_BASE_URL = 'https://https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    NEWS_API_BASE_URL = 'https://newsapi.org/v2/sources?language=en&category={}&apiKey={}'
+    EVERYTHING_SOURCE_BASE_URL = 'https://newsapi.org/v2/everything?language=en&category={}&apiKey={}'
+    NEWS_API_KEY = '212b0b391fb8496e859d04f7f34ed150'
+
 
 class ProdConfig(Config):
     '''
